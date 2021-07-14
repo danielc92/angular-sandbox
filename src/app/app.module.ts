ca7e3219607613426dc62533d0f23c596c57fa88
Daniel Corcoran <daniel.corcoran@weareyou.com.au>
2021-07-14 21:59:47 +1000

    add ttodo to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { LmaoComponent } from './lmao/lmao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    LmaoComponent
   ],
   imports: [
     BrowserModule,
