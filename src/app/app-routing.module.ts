68c40b456c91ae68cfc49c16d049909ba0562533
Daniel Corcoran <daniel.corcoran@weareyou.com.au>
2021-07-15 16:08:18 +1000

    routes configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TodoListPageComponent } from './todo-list-page/todo-list-page.component';
+const routes: Routes = [
+  {
+    path: 'about-us',
+    component: AboutUsPageComponent,
+  },
+  {
+    path: 'todo-list',
+    component: TodoListPageComponent,
+  },
+  {
+    path: 'profile/:profile_id',
+    component: ProfileComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
